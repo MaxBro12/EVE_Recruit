bac0c216671217d9be8e9cd5be9e904fa278a0af
Maxim Bodrov <activer12@mail.ru>
2023-02-14 19:29:33 +0300

    beta1

--- code/clipb.py
+++ code/clipb.py
@@ -8,3 +8,7 @@ def write_to_cb(data: str) -> str:
     copy(data)
     paste()
     return data
+
+
+def get_from_cp():
+    return paste()
--- code/main.py
+++ code/main.py
@@ -1,39 +1,45 @@
 from sys import argv
+import os
 
 from debug import create_log_file
 from start import main_cheack
 from mybase import Table
 from settings import csv_file
-from clipb import write_to_cb
+from clipb import write_to_cb, get_from_cp
 
 
-def main(args: list):
+def main(args: list = None):
     main_cheack()
     base = Table(csv_file)
     copy_msg = ''
 
     while True:
-        pilot = input(': ')
-        if pilot == '':
-            write_to_cb(copy_msg)
-            print(copy_msg)
-            print('Список пилотов для рассылки сохранен в буфер')
-        else:
-            if base.add(pilot):
-                print('НОВЫЙ ПИЛОТ: ' + pilot)
-                if copy_msg != '':
-                    copy_msg += ', ' + pilot
-                else:
-                    copy_msg = pilot
-            else:
-                print('УЖЕ В БАЗЕ: ' + pilot)
+        ans = input('Скопируйте содержимое чата и нажмите enter\n')
+        if ans == '':
+            pilots = get_from_cp()
+
+            if ',' in pilots:
+                continue
+
+            pilots = pilots.split('\n')
+
+            if len(pilots) > 0:
+                for pilot in pilots:
+                    if base.add(pilot):
+                        if copy_msg != '':
+                            copy_msg += ', ' + pilot
+                        else:
+                            copy_msg = pilot
+                print('Список пилотов для рассылки сохранен в буфер:')
+                write_to_cb(copy_msg)
+                print(copy_msg)
+                copy_msg = ''
+        elif ans == 'EXET':
+            break
 
 
 if __name__ == '__main__':
     try:
-        argv.pop(0)
-        if argv == []:
-            argv = None
         main(argv)
     except Exception as er:
         create_log_file(er)
